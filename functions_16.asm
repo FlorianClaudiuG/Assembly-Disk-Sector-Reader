afffc93717864c3d9cbf0312472047027b3827fb
Florian-Claudiu Gheorghica <claudiu_fg@yahoo.com>
2018-02-27 17:06:22 +0200

    Fixed formatting.

--- functions_16.asm
+++ functions_16.asm
@@ -3,12 +3,12 @@
 ; Output Carriage-Return/Line-Feed (CRLF) sequence to screen using BIOS
 
 Console_Write_CRLF:
-	mov 	ah, 0Eh						; Output CR
-    mov 	al, 0Dh
-    int 	10h
-    mov 	al, 0Ah						; Output LF
-    int 	10h
-    ret
+    	mov 		ah, 0Eh					; Output CR
+    	mov 		al, 0Dh
+    	int 		10h
+    	mov 		al, 0Ah					; Output LF
+    	int 		10h
+    	ret
 
 ; Write to the console using BIOS.
 ; 
@@ -18,11 +18,11 @@ Console_Write_16:
 	mov 	ah, 0Eh						; BIOS call to output value in AL to screen
 
 Console_Write_16_Repeat:
-	lodsb								; Load byte at SI into AL and increment SI
-    test 	al, al						; If the byte is 0, we are done
+	lodsb							; Load byte at SI into AL and increment SI
+    	test 		al, al					; If the byte is 0, we are done
 	je 		Console_Write_16_Done
-	int 	10h							; Output character to screen
-	jmp 	Console_Write_16_Repeat
+	int 		10h					; Output character to screen
+	jmp 		Console_Write_16_Repeat
 
 Console_Write_16_Done:
     ret
@@ -41,7 +41,7 @@ Console_Write_ASCII:
 
 Output_Underscore:
 	mov		ah, 0Eh
-	mov 	al,	5Fh
+	mov 		al, 5Fh
 	int		10h
 	ret
 
@@ -50,8 +50,8 @@ Output_Underscore:
 ; Input: SI points to a null-terminated string
 
 Console_WriteLine_16:
-	call 	Console_Write_16
-	call 	Console_Write_CRLF
+	call 		Console_Write_16
+	call 		Console_Write_CRLF
 	ret
 
 ; Write content of bx as an int to the screen.
@@ -65,17 +65,17 @@ Console_Write_Int:
 GetDigit:
 	xor		dx, dx
 	mov		cx, 10
-	div 	cx
+	div 		cx
 	add		dl, 48
 	mov		[si], dl
-	dec 	si
+	dec 		si
 	cmp		dx, 0
 	jne		GetDigit
-	inc 	si
-	call	Console_Write_16
+	inc 		si
+	call		Console_Write_16
 	ret
 	
-IntBuffer	db	'      ', 0
+IntBuffer	db '      ', 0
 
 ;Write contents of bx as a hexadecimal number to the screen.
 ;
@@ -95,8 +95,8 @@ HexLoop:
 	and		si, 000Fh
 	mov		al, byte[si + HexChars]
 	mov		ah, 0Eh
-	int 	10h
-	loop	HexLoop
+	int 		10h
+	loop		HexLoop
 
 Console_Write_Space:
 	mov		ah, 0Eh
